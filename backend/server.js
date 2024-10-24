658e0c8131c21199288b75453afc4183a0efa3e1
bhvyeahh <bhavyarathore575@gmail.com>
2024-10-24 11:53:18 +0530

    backend commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+// app config
+
+const app = express()
+
+const port = process.env.PORT || 4000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+
+app.get('/', (req, res)=>{
+    res.send('API Working here')
+})
+
+app.listen(port,()=> console.log("ServerStarted", port))
